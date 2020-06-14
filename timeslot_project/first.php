65f1a516dee0818a28fd7c9017546ed823b4b797
Virendar Singh <veerbeniwal96@gmail.com>
2020-06-14 19:49:35 +0530

    Adding project

---
+++ timeslot_project/first.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+	<div class="container">
+		<div class="header">
+			<h2>Register</h2>
+		</div>
+		<form action="registration.php method="post">
+		</form>	
